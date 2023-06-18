404b24b33a1a5502c206dab2e1bb3b1f48d77b82
Kostya <kostya173russ@gmail.com>
2023-06-18 11:16:11 +0300

    решены все задачи

---
+++ Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// [1,7] -> такого числа в массиве нет [1,7]-это позиция элемента 1 - строка, 7 - столбец
+int rows = Prompt("Введите количество строк массива: ");
+int columns = Prompt("Введите количество столбцов массива: ");
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+if (rows < array.GetLength(0) && columns < array.GetLength(1)) Console.WriteLine(array[rows, columns]);
+else Console.WriteLine($"{rows}{columns} числа в массиве нет");
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++) // GetLength(0) отвечает за строки
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int rows = Prompt("Введите количество строк массива: ");
+int columns = Prompt("Введите количество столбцов массива: ");
+double[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+double[] avgNumbers = new double[array.GetLength(1)];
+
+for (int i = 0; i < array.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        result += array[j, i];
+    }
+    avgNumbers[i] = result / array.GetLength(0);
+}
+PrintArray2(avgNumbers);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++) // GetLength(0) отвечает за строки
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+
+    }
+}
+void PrintArray2(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
